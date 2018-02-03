c5b725452665db2877ed676e6e70c8ff8b464bfc
Alex <bethazari@gmail.com>
2018-02-03 13:04:20 +0300

    storybook integration

--- src/widgets/sidebar.js
+++ src/widgets/sidebar.js
@@ -10,34 +10,52 @@ import {
 import InboxIcon from 'material-ui-icons/Inbox';
 import DraftsIcon from 'material-ui-icons/Drafts';
 
-export default () => (
-  <div>
-    <Hidden mdUp>
-      <Drawer
-        anchor="right"
-        type="temporary"
-      >
-        <List>
-          <ListItem>
-            <ListItemIcon><InboxIcon /></ListItemIcon>
-            <ListItemText primary="up" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </Hidden>
-    <Hidden smDown>
-      <Drawer
-        open
-        anchor="left"
-        type="permanent"
-      >
-        <List>
-          <ListItem>
-            <ListItemIcon><DraftsIcon /></ListItemIcon>
-            <ListItemText primary="down" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </Hidden>
-  </div>
-);
+export default class Sidebar extends React.Component {
+  show() {}
+
+  render() {
+    const brand = (
+      <div>
+        <a href="https://www.creative-tim.com">
+          <div>
+            <img src="http://" alt="logo" />
+          </div>
+          test
+        </a>
+      </div>
+    );
+    return (
+      <div>
+        <Hidden mdUp>
+          <Drawer
+            anchor="right"
+            type="temporary"
+          >
+            {brand}
+            <List>
+              <ListItem>
+                <ListItemIcon><InboxIcon /></ListItemIcon>
+                <ListItemText primary="up" />
+              </ListItem>
+            </List>
+          </Drawer>
+        </Hidden>
+        <Hidden smDown>
+          <Drawer
+            open
+            anchor="left"
+            type="permanent"
+          >
+            {brand}
+            <List>
+              <ListItem>
+                <ListItemIcon><DraftsIcon /></ListItemIcon>
+                <ListItemText primary="down" />
+              </ListItem>
+            </List>
+          </Drawer>
+        </Hidden>
+      </div>
+    );
+  }
+}
---
+++ src/widgets/sidebar.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Sidebar from './sidebar';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Sidebar onClick={action('clicked')} />
+  ))
+  .add('with some emoji', () => (
+    <Sidebar onClick={action('clicked')} />
+  ));
